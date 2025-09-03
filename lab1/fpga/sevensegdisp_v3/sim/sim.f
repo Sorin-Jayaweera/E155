ee051408c64cebd7a24ab4a67ed7a056e45e45f3
Sorin Jayaweera <sojayaweera@hmc.edu>
2025-09-02 19:56:16 -0700

    lab1 from lab comp

---
+++ lab1/fpga/sevensegdisp_v3/sim/sim.f
@@ -0,0 +1,25 @@
+-L work
+-reflib pmi_work
+-reflib ovi_ice40up
+
+
+"C:/Users/sojayaweera/E155/lab1/fpga/sevensegdisp_v3/source/impl_1/top.sv" 
+"C:/Users/sojayaweera/E155/lab1/fpga/sevensegdisp_v3/source/impl_1/testbench.sv" 
+-sv
+-optionset VOPTDEBUG
++noacc+pmi_work.*
++noacc+ovi_ice40up.*
+
+-vopt.options
+  -suppress vopt-7033
+-end
+
+-gui
+-top testbench
+-vsim.options
+  -suppress vsim-7033,vsim-8630,3009,3389
+-end
+
+-do "view wave"
+-do "add wave /*"
+-do "run 100 ns"
