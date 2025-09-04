7f6b35927bb840b36f31dfc66b44d9fb9aaa0daf
Sorin Jayaweera <sojayaweera@hmc.edu>
2025-09-04 16:52:32 -0700

    made pins and fixed TOP

--- lab2/dual_sevenseg/source/top/top.sv
+++ lab2/dual_sevenseg/source/top/top.sv
@@ -2,15 +2,17 @@ module top(
 	input logic [3:0] i0,
 	input logic [3:0] i1,
 	input logic reset,
-	output logic [6:0] seg0,
-	output logic [6:0] seg1,
+	output logic sel,
+	output logic nsel,
+	output logic [6:0] segout,
 	output logic [4:0] sum
 	);
 	
 	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 	
-	logic sel, nsel;
-	logic [6:0] segout = 7'b1111111;
+	logic [6:0] seg0 = 7'b1111111;
+	logic [6:0] seg1 = 7'b1111111;
+	
 	logic [3:0] iActive = 4'b0000;
 	
 	logic [24:0] counter = 0;
@@ -19,10 +21,11 @@ module top(
 	segLUT lut(.s(iActive),.seg(segout));
 	
 	// add the inputs and display on LEDS
-	fourbitadder adder(i0, i1, sum);
+	assign sum = i0+i1;//fourbitadder adder(i0, i1, sum);
 	
 	// choosing which set of connections for the resource use
-	always_ff@(sel, posedge reset) begin
+	// sequential logic
+	always_ff@(sel, reset) begin
 		
 			if(reset == 1) begin
 					iActive <= 4'b0000;
@@ -31,12 +34,12 @@ module top(
 				end
 			else begin
 				iActive <= sel ? i0 : i1;
-				seg0 <= sel ? segout : seg0;
+				seg0 <= sel ? segout : seg0; // previous value OR the new thing
 				seg1 <= sel ? seg1 : segout;
 				end
 	
 	end
-	
+	// select as a slow clock logic
 	always_ff@(posedge int_osc, reset) begin
 		if(reset) begin
 				counter <= 25'b0;
@@ -44,7 +47,7 @@ module top(
 		else begin
 			counter <= counter + 1'b1;
 		
-			sel <= counter[5];
+			sel <= counter[4];
 			nsel <= ~sel;
 		end
 		
