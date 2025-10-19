4e0786a7342733baa60af86e09dc1a25d2e8e65e
Sorin Jayaweera <sojayaweera@hmc.edu>
2025-10-19 12:18:33 -0700

    some mild work

--- lab6/tutorial-esp8266-click-webserver-main/SEGGER_proj/Output/Debug/Obj/ESP8266 Webserver Demo/system_stm32l4xx.d
+++ lab6/tutorial-esp8266-click-webserver-main/SEGGER_proj/Output/Debug/Obj/ESP8266 Webserver Demo/system_stm32l4xx.d
@@ -1,15 +1,15 @@
 Output/Debug/Obj/ESP8266\ Webserver\ Demo/system_stm32l4xx.o: \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Source\system_stm32l4xx.c \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Include\stm32l4xx.h \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Include\stm32l432xx.h \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\core_cm4.h \
-  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.22a\include\stdint.h \
-  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.22a\include\__SEGGER_RTL.h \
-  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.22a\include\__SEGGER_RTL_ConfDefaults.h \
-  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.22a\include\__SEGGER_RTL_Conf.h \
-  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.22a\include\__SEGGER_RTL_Arm_Conf.h \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\cmsis_version.h \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\cmsis_gcc.h \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\mpu_armv7.h \
-  C:\ThatFamily\ Dropbox\Sorin\ Jayaweera\allSaves\mudd\junior\MicroPs\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Include\system_stm32l4xx.h
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Source\system_stm32l4xx.c \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Include\stm32l4xx.h \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Include\stm32l432xx.h \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\core_cm4.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\stdint.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL_ConfDefaults.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL_Arm_Conf.h \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\cmsis_gcc.h \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\CMSIS_5\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\sojayaweera\E155\lab6\tutorial-esp8266-click-webserver-main\SEGGER_proj\STM32L4xx\Device\Include\system_stm32l4xx.h
