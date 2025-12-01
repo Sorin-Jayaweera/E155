6405aebc7789b0ea972cd362061eb60ed57c3c2c
Sorin Jayaweera <sojayaweera@hmc.edu>
2025-12-01 00:54:52 -0800

    leaving lab

--- project/stm/Output/Debug/Obj/FFT/STM32L432KC_GPIO.d
+++ project/stm/Output/Debug/Obj/FFT/STM32L432KC_GPIO.d
@@ -5,4 +5,5 @@ Output/Debug/Obj/FFT/STM32L432KC_GPIO.o: \
   C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL.h \
   C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL_ConfDefaults.h \
   C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL_Conf.h \
-  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL_Arm_Conf.h
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\__SEGGER_RTL_Arm_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.24\include\stdio.h
